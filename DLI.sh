5d2f38d170c2dcff60195730ef6f3348c1907ba1
Tim0Xa <kogtim@gmail.com>
2018-06-12 08:31:57 +0300

    First commit

---
+++ DLI.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+	# This script is used for quick Termux
+	# adjustment, and set ready to use!
+
+pkg install nano -y		# console text edito
+pkg install ssh -y		# ssh client
+pkg install python -y		# python 3.6
+pkg install clang -y		# C & C++ compiller
+pkg install tracepath -y	# internet utilities
+pkg install git -y		# GitHub client
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# TermuxSetUp
+
+This is a library of personal preferences for Android Termux app adjustment.
+
