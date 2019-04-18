d8c485467925dcdf195ffab567a035e6752ae1fc
Tim0Xa <kogtim@gmail.com>
2019-04-18 21:38:59 +0300

    Update DLI.sh

--- DLI.sh
+++ DLI.sh
@@ -3,23 +3,29 @@
 	# This script is used for quick Termux
 	# adjustment, and set ready to use!
 
+sleep 2s
 # console text edito
-sleep 5s
+sleep 3s
 pkg install nano -y
+sleep 2s
 # ssh client
-sleep 5s
+sleep 3s
 pkg install openssh -y
+sleep 2s
 # python 3.6
-sleep 5s
+sleep 3s
 pkg install python -y
+sleep 2s
 # C & C++ compiller
-sleep 5s
+sleep 3s
 pkg install clang -y
+sleep 2s
 # internet utilities
-sleep 5s
+sleep 3s
 pkg install tracepath -y
+sleep 2s
 # GitHub client
-sleep 5s
+sleep 3s
 pkg install git -y
 
 
