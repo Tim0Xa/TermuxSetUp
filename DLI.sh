dcf7d618a4ad5ee1115b07c91dcac6c7e1d568ab
Tim0Xa <kogtim@gmail.com>
2019-04-19 00:11:53 +0300

    Update DLI.sh

--- DLI.sh
+++ DLI.sh
@@ -4,27 +4,27 @@
 	# adjustment, and set ready to use!
 
 # console text edito nano
-printf "  text edito nano \n\n"
+printf "\n\n  text edito nano \n\n"
 sleep 2s
 pkg install nano -y
 # ssh client
-printf "  ssh client \n\n"
+printf "\n\n ssh client \n\n"
 sleep 2s
 pkg install openssh -y
 # python 3.6
-printf "  python 3.6 \n\n"
+printf "\n\n python 3.6 \n\n"
 sleep 2s
 pkg install python -y
 # C & C++ compiller
-printf "  C & C++ compiller \n\n"
+printf "\n\n C & C++ compiller \n\n"
 sleep 2s
 pkg install clang -y
 # internet utilities
-printf "  internet utilities \n\n"
+printf "\n\n internet utilities \n\n"
 sleep 2s
 pkg install tracepath -y
 # GitHub client
-printf "  GitHub client \n\n"
+printf "\n\n GitHub client \n\n"
 sleep 2s
 pkg install git -y
 
