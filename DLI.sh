b97b8bce2732e7e19f034ccd457cd76f340c3b0a
Tim0Xa <kogtim@gmail.com>
2019-04-19 00:45:53 +0300

    Update DLI.sh

--- DLI.sh
+++ DLI.sh
@@ -3,6 +3,12 @@
 	# This script is used for quick Termux
 	# adjustment, and set ready to use!
 
+# internet utilities
+printf "\n\n internet utilities \n\n"
+sleep 2s
+pkg install tracepath -y
+pkg install termux-api -y
+pkg install man -y
 # console text edito nano
 printf "\n\n  text edito nano \n\n"
 sleep 2s
@@ -19,10 +25,6 @@ pkg install python -y
 printf "\n\n C & C++ compiller \n\n"
 sleep 2s
 pkg install clang -y
-# internet utilities
-printf "\n\n internet utilities \n\n"
-sleep 2s
-pkg install tracepath -y
 # GitHub client
 printf "\n\n GitHub client \n\n"
 sleep 2s
