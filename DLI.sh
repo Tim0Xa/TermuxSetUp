d23796fed1b1edb19409035c4f85820434581ecf
Tim0Xa <kogtim@gmail.com>
2019-04-18 21:13:57 +0300

    Update DLI.sh

--- DLI.sh
+++ DLI.sh
@@ -4,15 +4,20 @@
 	# adjustment, and set ready to use!
 
 # console text edito
+sleep 3s
 pkg install nano -y
 # ssh client
+sleep 3s
 pkg install ssh -y
 # python 3.6
+sleep 3s
 pkg install python -y
 # C & C++ compiller
+sleep 3s
 pkg install clang -y
 # internet utilities
+sleep 3s
 pkg install tracepath -y
 # GitHub client
+sleep 3s
 pkg install git -y
-
